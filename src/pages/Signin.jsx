09390aaa40e4f6e405ce01ce3095a5a52c0b900f
piyushj25637 <piyush.4963@gmail.com>
2026-05-07 12:14:03 +0530

    Update Signin.jsx

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import './SignIn.css'
+import './Signin.css'
  
 function SignIn() {
   const [email, setEmail] = useState('')
